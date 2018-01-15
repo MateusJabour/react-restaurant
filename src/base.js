c02913b8b3547d1ed97f6c59bb3f099cc5b5337b
Mateus Jabour <mjabourbp@gmail.com>
2018-01-15 16:39:51 -0500

    initial commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBnQpFCJ0E4cCJgR7kjBJJAB8NnhZIm5sI",
+  authDomain: "catch-of-the-day-mateus.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-mateus.firebaseio.com",
+});
+
+export default base;
